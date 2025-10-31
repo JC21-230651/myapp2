9a3bf76927fe1cbdbd7676833a02e905d61af713
kuwakan1222 <kuwakan1222@gmail.com>
2025-10-31 01:58:20 +0000

    iOS関連追加3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,22 @@
-
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:myapp/my_app.dart';
+import 'package:myapp/theme_provider.dart';
 
 void main() {
   testWidgets('App starts and displays home page', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ],
+        child: MyApp(),
+      ),
+    );
 
-    // Verify that the home page is displayed.
+    await tester.pumpAndSettle();
+
     expect(find.byType(Scaffold), findsOneWidget);
   });
 }
